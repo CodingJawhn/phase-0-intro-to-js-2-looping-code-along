f62b9f064e7cc0c6e8c86c04eec62010b3173a3e
John Yang <john.yang8097@gmail.com>
2022-11-13 16:46:04 -0500

    commit tree dac1021526280dc1b76bad95becba1b9b778b51e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const phrase = `surprise`
+function writeCards(names, phrase) {
+    let thankYou = []
+    for (let i = 0; i < names.length; i++){
+       thankYou.push (`Thank you, ${names[i]}, for the wonderful ${phrase} gift!`)
+    }
+    return thankYou
+}
+
+function countDown(n) {
+    while (n > 0) {
+        console.log(n)
+        n -= 1
+    }
+    console.log(n)
+}
